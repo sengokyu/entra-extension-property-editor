6db43653424dec8dbd12c4a9712683e18911b21c
sengokyu <sengokyu+gh@gmail.com>
2024-09-08 22:49:08 +0900

    Update app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,12 +3,15 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { authConfig } from './auth/auth.config';
-import { provideAuth } from 'angular-auth-oidc-client';
+import { AuthInterceptor, provideAuth } from 'angular-auth-oidc-client';
+import { HTTP_INTERCEPTORS, provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAuth(authConfig),
+    provideHttpClient(),
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
   ],
 };
