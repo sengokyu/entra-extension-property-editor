deb563bdc8feabab75a4f79e5c1853c258c93bb2
sengokyu <sengokyu+gh@gmail.com>
2024-09-08 22:28:19 +0900

    Generate UnauthorizedComponent

---
+++ src/app/unauthorized/unauthorized.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-unauthorized',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      unauthorized works!
+    </p>
+  `,
+  styles: ``
+})
+export class UnauthorizedComponent {
+
+}
