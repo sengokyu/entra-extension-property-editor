910eaffb5e192b1a3d4510140b6a296885ad5177
sengokyu <sengokyu+gh@gmail.com>
2024-09-08 22:48:54 +0900

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AutoLoginPartialRoutesGuard } from 'angular-auth-oidc-client';
+import { CallbackComponent } from './callback/callback.component';
+import { UnauthorizedComponent } from './unauthorized/unauthorized.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home',
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+    canActivate: [AutoLoginPartialRoutesGuard],
+  },
+  {
+    path: 'callback',
+    component: CallbackComponent,
+  },
+  { path: 'unauthorized', component: UnauthorizedComponent },
+];
