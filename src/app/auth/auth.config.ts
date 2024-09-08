4432f8a793357eedc5870a282bfbb43e773fe60b
sengokyu <sengokyu+gh@gmail.com>
2024-09-08 22:47:09 +0900

    Update auth.config.ts

--- src/app/auth/auth.config.ts
+++ src/app/auth/auth.config.ts
@@ -1,21 +1,24 @@
-import { PassedInitialConfig } from 'angular-auth-oidc-client';
+import { LogLevel, PassedInitialConfig } from 'angular-auth-oidc-client';
 
 export const authConfig: PassedInitialConfig = {
   config: {
     authority:
       'https://login.microsoftonline.com/a7c09d81-121c-4816-b499-55e4f96b3b71/v2.0',
     authWellknownEndpointUrl: 'https://login.microsoftonline.com/common/v2.0',
-    redirectUrl: window.location.origin,
+    redirectUrl: window.location.origin + '/callback',
     clientId: '287ade13-1801-48a6-bb6d-f9b2493804cb',
     scope: 'openid .default', // 'openid profile offline_access ' + your scopes
     responseType: 'code',
     silentRenew: true,
     useRefreshToken: true,
     maxIdTokenIatOffsetAllowedInSeconds: 600,
-    issValidationOff: false,
+    // iss validationに失敗するのでoff
+    issValidationOff: true,
     autoUserInfo: false,
     customParamsAuthRequest: {
       prompt: 'select_account', // login, consent
     },
+    secureRoutes: ['https://graph.microsoft.com'],
+    logLevel: LogLevel.Debug,
   },
 };
