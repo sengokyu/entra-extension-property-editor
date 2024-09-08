ce11536245eea6440d463740a9986047cbf5c039
sengokyu <sengokyu+gh@gmail.com>
2024-09-08 22:49:23 +0900

    Update CallbackComponent

--- src/app/callback/callback.component.ts
+++ src/app/callback/callback.component.ts
@@ -1,16 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { OidcSecurityService } from 'angular-auth-oidc-client';
 
 @Component({
   selector: 'app-callback',
   standalone: true,
   imports: [],
-  template: `
-    <p>
-      callback works!
-    </p>
-  `,
-  styles: ``
+  template: ` <p>callback works!</p> `,
+  styles: ``,
 })
-export class CallbackComponent {
+export class CallbackComponent implements OnInit {
+  constructor(private readonly oidcSecurityService: OidcSecurityService) {}
 
+  ngOnInit(): void {
+    this.oidcSecurityService.checkAuth().subscribe((loginResponse) => {
+      console.log(loginResponse);
+    });
+  }
 }
